82e38ced6754a68b96c400a25432311f49460ad8
chauhan <akchauhan376@gmail.com>
2025-06-30 20:58:37 +0530

    Add commit 2 in task 2

--- gitcmdDEMO/src/main/java/com/example/pkg1/class3.java
+++ gitcmdDEMO/src/main/java/com/example/pkg1/class3.java
@@ -1,4 +1,8 @@
 package com.example.pkg1;
 
 public class class3 {
+
+    public void addInExisting(){
+        System.out.println("commit to revert from akash/task2");
+    }
 }
---
+++ gitcmdDEMO/src/main/java/com/example/task2/Task2.java
@@ -0,0 +1,7 @@
+package com.example.task2;
+
+public class Task2 {
+    public void printTask2(){
+        System.out.println("commit task 2 for branch: akash/task2");
+    }
+}
