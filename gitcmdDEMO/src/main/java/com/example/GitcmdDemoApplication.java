1ab4d311d5d22bda33b3334ca9044533bd30f0d3
chauhan <akchauhan376@gmail.com>
2026-02-22 20:54:49 +0530

    commit2

--- gitcmdDEMO/src/main/java/com/example/GitcmdDemoApplication.java
+++ gitcmdDEMO/src/main/java/com/example/GitcmdDemoApplication.java
@@ -9,6 +9,7 @@ public class GitcmdDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitcmdDemoApplication.class, args);
 		System.out.println("commit 1");
+		System.out.println("commit 2");
 	}
 
 }
